3fa37fd64083f283a0b463f5a05c35aa91b45163
Shashanka Venkataramanan <svenkata@meerqat.irisa.fr>
2022-04-01 21:29:42 +0200

    minor corrections

--- tiny_imgnet/submit_job.sh
+++ tiny_imgnet/submit_job.sh
@@ -15,13 +15,12 @@ echo " got the python script"
 module load pytorch/1.10.1-py3.8
 
 
-# EXECUTABLE= main.py  --train_dir /nfs/pyrex/raid6/svenkata/Datasets/tiny-imagenet-200/train \
-# 					--val_dir /nfs/pyrex/raid6/svenkata/Datasets/tiny-imagenet-200/val \
-# 					--save_dir /nfs/pyrex/raid6/svenkata/weights/AlignMixup_CVPR22/tiny_imagenet --epochs 1200 \
-# 					--alpha 2.0 --num_classes 200 --manualSeed 8492
+EXECUTABLE="main.py  --train_dir /nfs/pyrex/raid6/svenkata/Datasets/tiny-imagenet-200/train \
+					--val_dir /nfs/pyrex/raid6/svenkata/Datasets/tiny-imagenet-200/val \
+					--save_dir /nfs/pyrex/raid6/svenkata/weights/AlignMixup_CVPR22/tiny_imagenet --epochs 1200 \
+ 					--alpha 2.0 --num_classes 200 --manualSeed 8492"
 
 
-EXECUTABLE=main.py
 
 echo
 echo "=============== RUN ${OAR_JOB_ID} ==============="
